a6c06bc57a218f5dd4ce8aae87cfc97266c2153a
wenfeng <sxian.wang@gmail.com>
2024-04-30 01:03:55 -0500

    fix base path

--- next.config.js
+++ next.config.js
@@ -1,6 +1,7 @@
 const isProd = process.env.NODE_ENV === 'production'
 
 const withNextra = require('nextra')('nextra-theme-blog', './theme.config.js')
-module.exports = withNextra({
-  basePath: isProd ? '/blog' : ''
-})
+module.exports = withNextra()
+// module.exports = withNextra({
+//   basePath: isProd ? '/blog' : ''
+// })
