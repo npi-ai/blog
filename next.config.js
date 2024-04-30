f0d571645c4ed275533745eb88d105548b91d6e1
Daofeng Wu <dolphin.w.e@gmail.com>
2024-04-30 14:44:42 +0900

    build: use different base path for dev and prod

--- next.config.js
+++ next.config.js
@@ -1,2 +1,6 @@
+const isProd = process.env.NODE_ENV === 'production'
+
 const withNextra = require('nextra')('nextra-theme-blog', './theme.config.js')
-module.exports = withNextra()
+module.exports = withNextra({
+  basePath: isProd ? '/blog' : ''
+})
