7b7849c786163c552a0ad5f3906cf650eae8921a
wenfeng <sxian.wang@gmail.com>
2024-04-29 21:31:49 -0500

    add plausible

--- pages/_app.js
+++ pages/_app.js
@@ -2,26 +2,29 @@ import 'nextra-theme-blog/style.css'
 import Head from 'next/head'
 
 import '../styles/main.css'
+import PlausibleProvider from 'next-plausible'
 
 export default function Nextra({ Component, pageProps }) {
-  return (
-    <>
-      <Head>
-        {/*<link*/}
-        {/*  rel="alternate"*/}
-        {/*  type="application/rss+xml"*/}
-        {/*  title="RSS"*/}
-        {/*  href="/feed.xml"*/}
-        {/*/>*/}
-        <link
-          rel="preload"
-          href="/fonts/Inter-roman.latin.var.woff2"
-          as="font"
-          type="font/woff2"
-          crossOrigin="anonymous"
-        />
-      </Head>
-      <Component {...pageProps} />
-    </>
-  )
+    return (
+        <PlausibleProvider domain="blog.npi.ai">
+            <>
+                <Head>
+                    {/*<link*/}
+                    {/*  rel="alternate"*/}
+                    {/*  type="application/rss+xml"*/}
+                    {/*  title="RSS"*/}
+                    {/*  href="/feed.xml"*/}
+                    {/*/>*/}
+                    <link
+                        rel="preload"
+                        href="/fonts/Inter-roman.latin.var.woff2"
+                        as="font"
+                        type="font/woff2"
+                        crossOrigin="anonymous"
+                    />
+                </Head>
+                <Component {...pageProps} />
+            </>
+        </PlausibleProvider>
+    )
 }
